a4ea63c8f5caf304f53330f56a0569bdc3a8779e
leonardoitalo <131911745+leonardoitalo@users.noreply.github.com>
2024-01-03 02:38:29 -0300

    Configurando hosts

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -26,7 +26,9 @@ SECRET_KEY = "django-insecure-w#mhvfar_ug=s)k#&0182b+#f#i!)kwq=hg1n59$!ymj4ibl#v
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost",
+                 "127.0.0.1",
+                 "leonardoitalo.pythonanywhere.com"]
 
 
 # Application definition
