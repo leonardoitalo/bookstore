a27267202f19799b4b8fa663bbe14834fc625448
leonardoitalo <131911745+leonardoitalo@users.noreply.github.com>
2024-01-03 02:01:39 -0300

    Adicionando pagina html

--- bookstore/urls.py
+++ bookstore/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from bookstore import views
 from django.contrib import admin
 from django.urls import path, include, re_path
 from django.conf import settings
@@ -24,6 +25,7 @@ urlpatterns = [
     re_path("bookstore/(?P<version>(v1|v2))/", include("order.urls")),
     re_path("bookstore/(?P<version>(v1|v2))/", include("product.urls")),
     path("api-token-auth/", obtain_auth_token, name="api-token-auth"),
+    path("update_server/", views.update, name="update"),
 ]
 
 if settings.DEBUG:
